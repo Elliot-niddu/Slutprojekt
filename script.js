9e1334910dc193d336db6396be976ca51ae6aafb
Elliot-niddu <toggleobv@gmail.com>
2026-05-21 12:44:54 +0200

    hej

---
+++ script.js
@@ -0,0 +1,15 @@
+function checkAnswer(svar) {
+  if (svar === "rätt") {
+    document.getElementById("resultat").innerHTML = "Rätt svar!";
+  } else {
+    document.getElementById("resultat").innerHTML = "Fel svar, försök igen.";
+  }
+}
+
+function checkAnswer(svar) {
+  if (svar === "rätt") {
+    document.getElementById("resultat").innerHTML = "Rätt svar!";
+  } else {
+    document.getElementById("resultat").innerHTML = "Fel svar, försök igen.";
+  }
+}
