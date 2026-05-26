66af5507f88f1bde13aba81e164f98a9d39bbd45
Elliot-niddu <toggleobv@gmail.com>
2026-05-26 09:56:54 +0200

    hej

--- script.js
+++ script.js
@@ -8,8 +8,16 @@ function checkAnswer(svar) {
 
 function checkAnswer(svar) {
   if (svar === "rätt") {
-    document.getElementById("resultat").innerHTML = "Rätt svar!";
+    document.getElementById("resultatSol").innerHTML = "Rätt svar!";
   } else {
-    document.getElementById("resultat").innerHTML = "Fel svar, försök igen.";
+    document.getElementById("resultatSol").innerHTML = "Fel svar, försök igen.";
+  }
+}
+
+function checkAnswer(svar) {
+  if (svar === "rätt") {
+    document.getElementById("resultatIsotop").innerHTML = "Rätt svar!";
+  } else {
+    document.getElementById("resultatIsotop").innerHTML = "Fel svar, försök igen.";
   }
 }
